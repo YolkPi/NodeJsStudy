0455b79188ebc080046c20bf90b83d1c877bf568
YolkPi <nabulesideliming@gmail.com>
2020-07-14 00:15:23 -0700

    add npm package

---
+++ notes-app/app2.js
@@ -0,0 +1,8 @@
+//validator: npm package, check the validation and sanitation of string
+var vali = require('validator');
+//chalk: npm package, control output collor and style
+var chalk1 = require('chalk');
+//import vali2 from 'validator';
+
+//console.log(vali.isEmail('ww.s.add@cc.com'));
+console.log(chalk1.red.bgRed.bold(vali.isEmail('ww.s.add@cc.com')));
